a524c74ffffcab55b94b3230626df282d9ca8668
Alan Curtis <alan.curtis@bcfranchise.com>
2020-07-31 16:36:35 -0600

    test hit to grapql

--- src/pages/Page01/Page01.js
+++ src/pages/Page01/Page01.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Page01 = () => {
-  return <div className="Page01">Page01</div>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/graphql?query={customers{name,age}}`)
+      .then((res) => {
+        setUsers(res.data.data.customers);
+      });
+  }, []);
+
+  return (
+    <div className="Page01">
+      Page01
+      <div>
+        {users.map((user, i) => (
+          <div key={i}>{user.name}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Page01;
